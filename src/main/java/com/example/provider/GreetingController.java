7b35e4f86df333b8085475d8a241ef97643d323c
Adam-Lemdani <Adam.Lemdani@ii.co.uk>
2026-08-04 11:35:23 +0100

    demo-2

--- src/main/java/com/example/provider/GreetingController.java
+++ src/main/java/com/example/provider/GreetingController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-  
+
     @PostMapping("/api/greetings")
     public GreetingResponse greeting(@RequestBody GreetingRequest request) {
-        return new GreetingResponse("Hello " + request.name());
+        return new GreetingResponse("Hello " + request.fullName());
     }
 
 }
